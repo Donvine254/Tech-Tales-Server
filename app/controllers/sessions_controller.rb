f1cf84ffce66904f7868e0c2ad4ad7894c8d8b0f
donvine mugendi <donvinemugendi@gmail.com>
2023-08-23 22:28:24 +0300

    updated log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,12 +13,24 @@ class SessionsController < ApplicationController
         render json: { error: "Wrong password, please try again." }, status: :unauthorized
       end
     end
-  
+    def destroy
+      # # get the user with the specified ID
+      # user = User.find(params[:id])
+      # remove the user from the session
+      session.delete(:user_id)
+      session.delete(:username)
+      @_current_user = nil
+      render json: { message: 'Logged out successfully'}, status: :unauthorized
+    end
     private
   
     def render_not_found
       render json: { error: "User not found" }, status: :not_found
     end 
+    def current_user
+      @_current_user ||= session[:user_id] &&
+        User.find_by(id: session[:user_id])
+    end
   end
   
   
