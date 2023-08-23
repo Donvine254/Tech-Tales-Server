995a0fab546d2912c4357376bbadf9ca9b5c1ff6
donvine mugendi <donvinemugendi@gmail.com>
2023-08-23 17:04:50 +0300

    updated throttle

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,11 +6,14 @@ Rack::Attack.throttle('login/email', limit: 3, period: 60.seconds) do |req|
   end
 end
 
-Rack::Attack.throttle("requests by ip", limit: 20, period: 1.minutes) do |request|
+Rack::Attack.throttle("requests by ip", limit: 10, period: 1.minutes) do |request|
   request.ip unless request.get?
 end
+Rack::Attack.throttle("requests by ip", limit: 20, period: 1.minutes) do |request|
+  request.ip
+end
 
-throttle("logins/email", :limit => 3, :period => 60.seconds) do |req|
+Rack::Attack.throttle("logins/email", :limit => 3, :period => 60.seconds) do |req|
   if req.path == '/login' && req.post?
     # return the email if present, nil otherwise
     req.params['email'].presence
