d7828dd4eac4908128d46497b68a4c5a98db1cf8
Important Person <126117678+Donvine254@users.noreply.github.com>
2023-12-09 01:37:51 +0300

    add picture to users model

--- db/migrate/20230814114305_create_users.rb
+++ db/migrate/20230814114305_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :username
       t.string :password_digest, null: false
       t.string :email
+      t.string :picture
       t.timestamps
     end
   end
