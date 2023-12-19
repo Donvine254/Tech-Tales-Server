dbd661b8acd1cafac630ddc4228f2862bb6429e4
Donvine Mugendi <donvinemugendi@gmail.com>
2023-12-20 02:23:48 +0300

    update password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :comments
   validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A(.+)@(.+)\z/, message: 'Email invalid' },
                     length: { minimum: 4, maximum: 254 }
-  validates_presence_of :username, :password, :email
+  validates_presence_of :username, :email
   validates :password, length: { minimum: 8 }, presence: true, if: :password_digest_changed?
 end
