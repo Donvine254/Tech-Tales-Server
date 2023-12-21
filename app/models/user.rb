6f77f872884b8caffe8c30fe065746ebd80b685a
Donvine Mugendi <donvinemugendi@gmail.com>
2023-12-21 23:32:37 +0300

    add dependent destroy for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :blogs
-  has_many :comments
+  has_many :blogs, depedent: :destroy
+  has_many :comments, depedent: :destroy
   validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A(.+)@(.+)\z/, message: 'Email invalid' },
                     length: { minimum: 4, maximum: 254 }
   validates_presence_of :username, :email
