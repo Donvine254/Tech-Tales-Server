31519edb12c71ae1a740a61e55151fc92dce1344
Important Person <donvinemugendi@gmail.com>
2024-07-06 03:09:13 +0300

    Update blog_serializer.rb

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,5 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :slug, :image, :created_at_date, :author, :tags, :user_id, :user_avatar, :user_bio
+  attributes :id, :title, :body, :slug, :image, :created_at_date, :author, :tags, :user_id, :user_avatar, :user_bio, :author_socials
   has_many :comments
 
   def created_at_date
@@ -13,6 +13,9 @@ class BlogSerializer < ActiveModel::Serializer
   end
   def user_bio
     object.user.bio
+  end
+  def author_socials
+    object.user.socials
   end
 def user_avatar
 object.user.picture
