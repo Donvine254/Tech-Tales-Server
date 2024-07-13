ab35e21153a46709859478725fdf1fe37fedc100
Important Person <donvinemugendi@gmail.com>
2024-07-13 13:53:36 +0300

    Update social_medium.rb

--- app/models/social_medium.rb
+++ app/models/social_medium.rb
@@ -1,6 +1,6 @@
 class SocialMedium < ApplicationRecord
     belongs_to :user
     validates_presence_of :user_id, :platform, :url
-    validates_uniqueness_of :platform
+    validates_uniqueness_of :platform, scope: :user_id
     validates_uniqueness_of :url, scope: :platform
 end
